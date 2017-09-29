9b86d756981d06121901659101bad23d4c35ade8
Alex Suraci <suraci.alex@gmail.com>
2017-09-29 11:39:41 -0400

    render sections in parallel
    doing this brought Concourse's docs building time down from 12.8 seconds
    to 2.3.

--- render/writer.go
+++ render/writer.go
@@ -37,12 +37,20 @@ func (writer Writer) WriteSection(section *booklit.Section) error {
 		}
 	}
 
+	errs := make(chan error, len(section.Children))
 	for _, child := range section.Children {
-		err := writer.WriteSection(child)
+		go func() {
+			errs <- writer.WriteSection(child)
+		}()
+	}
+
+	var anyErr error
+	for i := 0; i < len(section.Children); i++ {
+		err := <-errs
 		if err != nil {
-			return err
+			anyErr = err
 		}
 	}
 
-	return nil
+	return anyErr
 }
