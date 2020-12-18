0e6efe6d790fee0d991fc73b0e11d75dbfbb4f92
Alex Suraci <suraci.alex@gmail.com>
2020-12-18 15:03:41 -0500

    godoc load package
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- load/processor.go
+++ load/processor.go
@@ -1,3 +1,6 @@
+// Package load glues everything together to provide a higher-level interfaces
+// for loading Booklit documents into Sections, either from files or from
+// already-parsed nodes (i.e. for inline sections).
 package load
 
 import (
@@ -12,6 +15,11 @@ import (
 	"github.com/vito/booklit/stages"
 )
 
+// Processor is a long-lived object for loading sections and evaluating
+// content.
+//
+// Document parsing is cached based on file modification time to avoid repeated
+// parsing of sub-sections when section content changes.
 type Processor struct {
 	AllowBrokenReferences bool
 
@@ -24,10 +32,17 @@ type parsedNode struct {
 	ModTime time.Time
 }
 
+// LoadFile parses the file at the given path and runs the three stages to
+// yield a Section.
 func (processor *Processor) LoadFile(path string, pluginFactories []booklit.PluginFactory) (*booklit.Section, error) {
 	return processor.LoadFileIn(nil, path, pluginFactories)
 }
 
+// LoadFileIn parses the file at the given path and runs the evaluate, collect,
+// and resolve stages to yield a Section.
+//
+// The given parent section is assigned as the parent of the new section so
+// that tags may resolve using the parent.
 func (processor *Processor) LoadFileIn(parent *booklit.Section, path string, pluginFactories []booklit.PluginFactory) (*booklit.Section, error) {
 	section, err := processor.EvaluateFile(parent, path, pluginFactories)
 	if err != nil {
@@ -37,6 +52,10 @@ func (processor *Processor) LoadFileIn(parent *booklit.Section, path string, plu
 	return processor.runStages(section)
 }
 
+// EvaluateFile parses the file at the given path and evaluates it, returning a
+// new section with given parent as its Parent.
+//
+// The returned section will not have been collected or resolved.
 func (processor *Processor) EvaluateFile(parent *booklit.Section, path string, pluginFactories []booklit.PluginFactory) (*booklit.Section, error) {
 	info, err := os.Stat(path)
 	if err != nil {
@@ -119,6 +138,10 @@ func (processor *Processor) EvaluateFile(parent *booklit.Section, path string, p
 	return section, nil
 }
 
+// EvaluateNode evaluates the given node and returns a new section with the
+// given parent as its Parent.
+//
+// The returned section will not have been collected or resolved.
 func (processor *Processor) EvaluateNode(parent *booklit.Section, node ast.Node, pluginFactories []booklit.PluginFactory) (*booklit.Section, error) {
 	section := &booklit.Section{
 		Parent: parent,
